75ea0e6b24ae86c94d0d82b730858c7fc0a6141e
alisu99 <alissonsts910@gmail.com>
2023-08-10 00:08:15 -0300

    atualizações

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def inicio():
     return render_template('inicio.html')
+
+@app.route('/servicos')
+def servicos():
+    return render_template('servicos.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
